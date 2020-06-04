6c94f593f94edc8fe2a84e9ec145dd3db7f94761
Shagun Agrawal <59168880+shagunagra@users.noreply.github.com>
2020-06-04 06:27:30 -0600

    Functions are given to the comment and cancel button.

--- src/Comment_section/Comment.js
+++ src/Comment_section/Comment.js
@@ -1,25 +1,60 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import './Comment.css';
 
 class Comment extends Component{
+
+
+    constructor(){
+        super();
+        this.state={
+            id: 0,
+            name: '',
+            comment: '',
+            show:false
+        }
+    }
+
+    inputChangedHandler = (e) =>{
+        const state=this.state;
+        state[e.target.name]=e.target.value;
+        this.setState(state);
+
+    }
+
+    handleClick = () => {
+        ReactDOM.findDOMNode(this.refs.form1).value = "";
+        ReactDOM.findDOMNode(this.refs.form2).value = "";
+      }
+
     render(){
+        const {name,comment}=this.state;
         return(
             <div>
+
+
                 <div className="comment_content">
                     <p className="comment_section">Comments</p>
                     <form className="form">
                         <div className="input_content">
                             <div>
-                                <input type="text" className="your_name" placeholder="Your Name" id="name"/>
+                                <input type="text" ref="form1" className="your_name" placeholder="Your Name" id="name" name="name" onChange={this.inputChangedHandler}/>
                             </div>
                             <div>
-                                <input type="text" className="your_comment" placeholder="Your Comment" id="comment"/>
+                                <input type="text" ref="form2" className="your_comment" placeholder="Your Comment" id="comment" name="comment" onChange={this.inputChangedHandler}/>
                             </div>
                         </div>
                     </form>
-                    <button className="comment_btn" id="comment">Comment</button>
-                    <button className="cancel_btn" id="cancel">Cancel</button>
+                    <button className="comment_btn" id="comment" onClick={()=>{this.setState({show:!this.state.show})}}>Comment</button>
+                    <button className="cancel_btn" id="cancel" onClick={this.handleClick}>Cancel</button>
                 </div>
+                {
+                    this.state.show? <div id="comment_form">
+                        <span className="icon"><i class="fa fa-user" aria-hidden="true"></i></span>
+                        <span className="get_name"> {name} </span><br/>
+                        <span className="get_comment"> {comment} </span>
+                    </div>: null
+                }
             </div>
         )
     }
