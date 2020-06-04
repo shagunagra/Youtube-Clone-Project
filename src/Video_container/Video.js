6188de6760bcc7b474a28f2493221eb97eb39364
Shagun Agrawal <59168880+shagunagra@users.noreply.github.com>
2020-06-04 04:05:12 -0600

    created video.js file.

---
+++ src/Video_container/Video.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class Videos extends Component{
+    render(){
+        return(
+            <div>
+                <div className="video_container">
+                    <div className="left_videoshow"></div>
+                    <div className="center_videoshow"></div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Videos;
