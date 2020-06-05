5f3e44020cc113ddf57106a9e978d3d72d75b549
Shagun Agrawal <59168880+shagunagra@users.noreply.github.com>
2020-06-05 07:58:03 -0600

    Functions for input is added.

--- src/Navigation_panel/Navigation.js
+++ src/Navigation_panel/Navigation.js
@@ -2,10 +2,21 @@ import React, {Component} from 'react';
 import './Navigation.css';
 
 class Navigation extends Component{
+
+    constructor(){
+        super();
+        this.state ={
+            videoname: ''
+        }
+    }
+
+    getVideoHandler = event => {
+        this.setState({videoname: event.target.value});
+    }
     render(){
         return(
             <div>
-                <input id="search" type="text" className="search_content" defaultValue="Search"/>
+                <input id="search" type="text" className="search_content" defaultValue="Search" onChange={this.getVideoHandler}/>
                 <button className="search_button" id="button">Search</button>
                 <hr className="line"/>
             </div>
