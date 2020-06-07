e238683706fb8f1d9ee23be0fc6f2f3c8b492304
Shagun Agrawal <59168880+shagunagra@users.noreply.github.com>
2020-06-07 13:46:44 -0600

    The resources of the data is chanded and the page is more structured.

--- src/Video_container/Video.js
+++ src/Video_container/Video.js
@@ -5,17 +5,38 @@ import 'font-awesome/css/font-awesome.min.css';
 
 class Videos extends Component{
 
-    getRedHandler(){
-        document.getElementById('heart').style.display='<i class="fa fa-heart" aria-hidden="true"></i>';
+    constructor() {
+        super();
+        this.state = {
+           videoDetail: []
+        }
     }
 
-    
+    getRedHandler(){
+        document.getElementById('heart').innerHTML="<i class='fa fa-heart' aria-hidden='true' id='redheart'></i>";
+    }
+
+    componentWillMount() {
+        let data = null;
+        let xhr = new XMLHttpRequest();
+        let that = this;
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                that.setState({
+                    upcomingMovies: JSON.parse(this.responseText).items
+                });
+            }
+        });
+        xhr.open("GET", "http://3.227.145.17:8085/api/v1/movies?page=1&limit=10");
+        xhr.setRequestHeader("Cache-Control","no-cache");
+        xhr.send(data);
+    }
     render(){
         return(
             <div>
                 <div className="main_container">
                     <div className="video_show">
-                        <iframe src='https://www.youtube.com/embed/E7wJTI-1dvQ'
+                        <iframe className="frame" src='http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com'
                            frameBorder='0'
                            allow='autoplay; encrypted-media'
                            allowFullScreen
@@ -23,11 +44,75 @@ class Videos extends Component{
                          <br/>
                         <span className="title" >Hello I am the title</span><br/>
                         <span className="channel_name">The Channel</span>
-                        <span onClick={this.getRedHandler} id="heart"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+                        <span onClick={this.getRedHandler} id="heart"><i className="fa fa-heart-o" aria-hidden="true"></i></span>
+                    </div>
+
+                    <div className="video_list">
+                        <div className="section_show">
+                            <iframe className="small_videos" src='http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com'
+                                frameBorder='0'
+                                allow='autoplay; encrypted-media'
+                                allowFullScreen
+                                title='video'/>
+                                <div className="break">
+                                    <span className="side_title" >Hello </span>
+                                    <span className="side_channel">The</span>
+                                </div>
+    
+                        </div>
+
+                        <div className="section_show">
+                            <iframe className="small_videos" src='http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com'
+                                frameBorder='0'
+                                allow='autoplay; encrypted-media'
+                                allowFullScreen
+                                title='video'/>
+                             <div className="break">
+                                    <span className="side_title" >Hello </span>
+                                    <span className="side_channel">The</span>
+                             </div>
+                        </div>
+
+                        <div className="section_show">
+                            <iframe className="small_videos" src='http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com'
+                                frameBorder='0'
+                                allow='autoplay; encrypted-media'
+                                allowFullScreen
+                                title='video'/>
+                                 <div className="break">
+                                    <span className="side_title" >Hello </span>
+                                    <span className="side_channel">The</span>
+                                </div>
+                        </div>
+
+                        <div className="section_show">
+                            <iframe className="small_videos" src='http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com'
+                                frameBorder='0'
+                                allow='autoplay; encrypted-media'
+                                allowFullScreen
+                                title='video'/>
+                                 <div className="break">
+                                    <span className="side_title" >Hello </span>
+                                    <span className="side_channel">The</span>
+                                </div>
+                        </div>
+
+                        <div className="section_show">
+                            <iframe className="small_videos" src='http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com'
+                                frameBorder='0'
+                                allow='autoplay; encrypted-media'
+                                allowFullScreen
+                                title='video'/>
+                                 <div className="break">
+                                    <span className="side_title" >Hello </span>
+                                    <span className="side_channel">The</span>
+                                </div>
+                        </div>
 
                     </div>
-                    <div className="video_list">
-                    </div>
+
+
+
                 </div>
             </div>
         )
