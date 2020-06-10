3a1008efbfbbd733d9782412b01fe3e3fc76a071
Shagun Agrawal <59168880+shagunagra@users.noreply.github.com>
2020-06-10 08:33:03 -0600

    Added the component for the grid.

--- src/Video_container/Video.js
+++ src/Video_container/Video.js
@@ -8,9 +8,12 @@ class Videos extends Component{
     constructor() {
         super();
         this.state = {
-           videoDetail: []
+           videoDetail: [{}],
+           videoTitle: [],
+           channel_title: []
         }
     }
+
 
     getRedHandler(){
         document.getElementById('heart').innerHTML="<i class='fa fa-heart' aria-hidden='true' id='redheart'></i>";
@@ -20,96 +23,68 @@ class Videos extends Component{
         let data = null;
         let xhr = new XMLHttpRequest();
         let that = this;
+        let url= "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&order=viewCount&q=trending&type=video&videoDefinition=high&key=AIzaSyBdNT43hUJh2UbaJg47Rhifa8wWLxElbM8";
+
         xhr.addEventListener("readystatechange", function () {
-            if (this.readyState === 4) {
-                that.setState({
-                    upcomingMovies: JSON.parse(this.responseText).items
-                });
+            if (this.readyState === 4 && this.status === 200) {
+
+                that.setState({videoDetail : JSON.parse(this.responseText).items[0],
+                    videoTitle: JSON.parse(this.responseText).items[0].snippet.title,
+                    channel_title: JSON.parse(this.responseText).items[0].snippet.channelTitle })
+                    //console.log(that.state.videoTitle);
+                var a= JSON.parse(this.responseText).items;
+                let arr =[]; 
+                for(var i=0;i<a.length;i++)
+                {
+                    arr.push(a[i].snippet.title);
+                }
+                console.log(arr);
+
             }
-        });
-        xhr.open("GET", "http://3.227.145.17:8085/api/v1/movies?page=1&limit=10");
-        xhr.setRequestHeader("Cache-Control","no-cache");
+        })
+        xhr.open("GET",url,true);
         xhr.send(data);
     }
+
+
+    
+
     render(){
         return(
             <div>
                 <div className="main_container">
                     <div className="video_show">
-                        <iframe className="frame" src='http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com'
+                        <iframe className="frame" src='http://www.youtube.com/embed/nqzIQh2D_Es'
                            frameBorder='0'
                            allow='autoplay; encrypted-media'
                            allowFullScreen
                            title='video'/>
                          <br/>
-                        <span className="title" >Hello I am the title</span><br/>
-                        <span className="channel_name">The Channel</span>
+                        <span className="title" >{this.state.videoTitle}</span><br/>
+                        <span className="channel_name">{this.state.channel_title}</span>
                         <span onClick={this.getRedHandler} id="heart"><i className="fa fa-heart-o" aria-hidden="true"></i></span>
                     </div>
 
+
                     <div className="video_list">
-                        <div className="section_show">
-                            <iframe className="small_videos" src='http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com'
-                                frameBorder='0'
-                                allow='autoplay; encrypted-media'
-                                allowFullScreen
-                                title='video'/>
-                                <div className="break">
-                                    <span className="side_title" >Hello </span>
-                                    <span className="side_channel">The</span>
-                                </div>
-    
-                        </div>
-
-                        <div className="section_show">
-                            <iframe className="small_videos" src='http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com'
-                                frameBorder='0'
-                                allow='autoplay; encrypted-media'
-                                allowFullScreen
-                                title='video'/>
-                             <div className="break">
-                                    <span className="side_title" >Hello </span>
-                                    <span className="side_channel">The</span>
-                             </div>
-                        </div>
-
-                        <div className="section_show">
-                            <iframe className="small_videos" src='http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com'
-                                frameBorder='0'
-                                allow='autoplay; encrypted-media'
-                                allowFullScreen
-                                title='video'/>
-                                 <div className="break">
-                                    <span className="side_title" >Hello </span>
-                                    <span className="side_channel">The</span>
-                                </div>
-                        </div>
-
-                        <div className="section_show">
-                            <iframe className="small_videos" src='http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com'
-                                frameBorder='0'
-                                allow='autoplay; encrypted-media'
-                                allowFullScreen
-                                title='video'/>
-                                 <div className="break">
-                                    <span className="side_title" >Hello </span>
-                                    <span className="side_channel">The</span>
-                                </div>
-                        </div>
-
-                        <div className="section_show">
-                            <iframe className="small_videos" src='http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com'
-                                frameBorder='0'
-                                allow='autoplay; encrypted-media'
-                                allowFullScreen
-                                title='video'/>
-                                 <div className="break">
-                                    <span className="side_title" >Hello </span>
-                                    <span className="side_channel">The</span>
-                                </div>
-                        </div>
-
+                        {
+                            this.state.sucess === "true" && this.state.searchpress === "true" &&
+                            <section>
+                                {this.state.videoDetail.map((getData)=>(
+                                    <div>
+                                        <section>
+                                            <p>
+                                                <span>{getData.snippet.title}<span>{getData.snippet.channelTitle}</span></span>
+                                            </p>
+                                            <br/><br/>
+                                        </section>
+                                        <img id={getData.id.videoId} src={getData.snippet.thumbnails.high.url}/>
+                                    </div>
+                                ))}
+                            </section>
+                        }
                     </div>
+
 
 
 
