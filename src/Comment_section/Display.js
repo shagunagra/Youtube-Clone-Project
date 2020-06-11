003158561d8da9431650d4a451aadc9b3082a8cc
Shagun Agrawal <59168880+shagunagra@users.noreply.github.com>
2020-06-11 12:18:38 -0600

    bool_state name changed.

--- src/Comment_section/Display.js
+++ src/Comment_section/Display.js
@@ -3,12 +3,12 @@ import React from 'react';
 function Display(props){
 
     const { people } = props;
-    const {bool_state} = props;
+    const {set_bool} = props;
     
     const peopleList = people.map( (person) => {
         return(
             <div id="comment_form" key={person.id}>
-                {bool_state === "true" &&
+                {set_bool === "true" &&
                  <span className="icon"><i class="fa fa-user" aria-hidden="true"></i></span>
                 }
                <span className="get_name"> {person.name} </span><br/>
