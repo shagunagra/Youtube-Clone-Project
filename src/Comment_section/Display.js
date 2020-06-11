13c46630f09d260a014856adcfa8b2a57d580710
Shagun Agrawal <59168880+shagunagra@users.noreply.github.com>
2020-06-11 10:15:39 -0600

    new file added

---
+++ src/Comment_section/Display.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Display(props){
+
+    const { people } = props;
+    
+    const peopleList = people.map( (person) => {
+        return(
+            <div id="comment_form" key={person.id}>
+               <span className="icon"><i class="fa fa-user" aria-hidden="true"></i></span>
+               <span className="get_name"> {person.name} </span><br/>
+               <span className="get_comment"> {person.comment} </span>
+            </div>
+        );
+    })
+
+    return(
+        <div className="display">
+            { peopleList }
+        </div>
+    );
+}
+
+export default Display;
