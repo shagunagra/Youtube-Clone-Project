6e95b8944ce53989a9149a80cb396a44a0728f44
Shagun Agrawal <59168880+shagunagra@users.noreply.github.com>
2020-06-11 10:16:51 -0600

    updates are done

--- src/Navigation_panel/Navigation.js
+++ src/Navigation_panel/Navigation.js
@@ -1,24 +1,21 @@
 import React, {Component} from 'react';
 import './Navigation.css';
+import ReactDOM from 'react-dom';
+import Videos from '../Video_container/Video';
+
 
 class Navigation extends Component{
 
-    constructor(){
-        super();
-        this.state ={
-            videoname: ''
-        }
-    }
-
-    getVideoHandler = event => {
-        this.setState({videoname: event.target.value});
+    gotHandler = (e) =>{
+        ReactDOM.render(<Videos/>,document.getElementById('root'));
     }
     render(){
         return(
             <div>
-                <input id="search" type="text" className="search_content" defaultValue="Search" onChange={this.getVideoHandler}/>
-                <button className="search_button" id="button">Search</button>
-                <hr className="line"/>
+        
+                    <input id="search" type="text" className="search_content" placeholder="Search"/>
+                    <button onClick={this.gotHandler} className="search_button" id="button">Search</button>
+                    <hr className="line"/>
             </div>
         )
     }
