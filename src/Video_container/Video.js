3e51ab72694fc22a3841c4528ef4e4d1d52fbbaf
Shagun Agrawal <59168880+shagunagra@users.noreply.github.com>
2020-06-11 10:17:24 -0600

    updates are done.

--- src/Video_container/Video.js
+++ src/Video_container/Video.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './Video.css';
 import 'font-awesome/css/font-awesome.min.css';
-import Comment from '../Comment_section/Comment';
+import Comment from '../Comment_container/Comment';
 import Navigation from '../Navigation_panel/Navigation';
 
 
@@ -10,8 +10,12 @@ class Videos extends Component{
     constructor() {
         super();
         this.state = {
-           videoDetail: [{}],
+           videoDetail: ' ',
            search_bool: 'false',
+           videoid: '9LtH9CsbRe4',
+           basicUrl: 'http://www.youtube.com/embed',
+           get_tile: 'Aage rehne ki aadat | #RahoAmbitious with upGrad',
+           get_channel: 'upGrad',
            success: "false"
         }
     }
@@ -21,21 +25,29 @@ class Videos extends Component{
         document.getElementById('heart').innerHTML="<i class='fa fa-heart' aria-hidden='true' id='redheart'></i>";
     }
 
-    componentWillMount() {
+    componentDidMount() {
         let data = null;
         let xhr = new XMLHttpRequest();
         let that = this;
-        let url= "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&order=viewCount&q=UpGrad&type=video&videoDefinition=high&key=AIzaSyBdNT43hUJh2UbaJg47Rhifa8wWLxElbM8";
+        let url= "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&order=viewCount&q=UpGrad&type=video&videoDefinition=high&key=AIzaSyDR4oBOsELhUm7dj-PZLsO7EnUfTPdpSU4";
         xhr.addEventListener("readystatechange", function () {
             if (this.readyState === 4 && this.status === 200) {
 
                 console.log(JSON.parse(this.responseText));
-                that.setState({videoDetail : JSON.parse(this.responseText).items[0]});
+                that.setState({videoDetail : JSON.parse(this.responseText).items});
+                that.setState({search_bool: "true"});
+                that.setState({success: "true"});
             }
         })
         xhr.open("GET",url,true);
         xhr.setRequestHeader("Cache-Control", "no-cache");
         xhr.send(data);
+    }
+
+    videoClickHandler =(id,titles,cha)=>{
+        this.setState({videoid: id});
+        this.setState({get_tile: titles});
+        this.setState({get_channel: cha});
     }
 
 
@@ -45,14 +57,14 @@ class Videos extends Component{
                  <Navigation/>
                 <div className="main_container">
                     <div className="video_show">
-                        <iframe className="frame" src='http://www.youtube.com/embed/nqzIQh2D_Es'
+                        <iframe className="frame" src={this.state.basicUrl + "/" + this.state.videoid}
                            frameBorder='0'
                            allow='autoplay; encrypted-media'
                            allowFullScreen
                            title='video'/>
                          <br/>
-                        <span className="title" >{this.state.videoTitle}</span><br/>
-                        <span className="channel_name">{this.state.channel_title}</span>
+                        <span className="title" >{this.state.get_tile}</span><br/>
+                        <span className="channel_name">{this.state.get_channel}</span>
                         <span onClick={this.getRedHandler} id="heart"><i className="fa fa-heart-o" aria-hidden="true"></i></span>
                         <Comment/>
                     </div>
@@ -63,7 +75,7 @@ class Videos extends Component{
                             <section>
                             {this.state.videoDetail.map((getData)=>(
                                 <div key={getData.id} style={{display: "flex", flexDirection: "row", marginTop: "40px"}}>
-                                    <img id={getData.id.videoId} src={getData.snippet.thumbnails.high.url} alt="anyname" style={{height: "120px", width: "140px", marginLeft: "25px"}} />
+                                    <img id={getData.id.videoId} src={getData.snippet.thumbnails.high.url} alt="anyname" style={{height: "120px", width: "140px", marginLeft: "25px", cursor: "pointer"}} onClick={this.videoClickHandler.bind(this, getData.id.videoId,getData.snippet.title,getData.snippet.channelTitle)}/>
                                     <section style={{position: "relative", top: "0px"}}>
                                         <p style={{position: "absolute", width: "242px"}}>
                                             <span style={{fontWeight: "bolder",marginLeft: "20px", position: "absolute"}}>{getData.snippet.title}<br/><span style={{color: "grey", fontWeight: "bold"}}>{getData.snippet.channelTitle}</span></span>
@@ -74,7 +86,7 @@ class Videos extends Component{
                             ))}
                         </section>
                         }
-                        {this.state.success === "true" && this.state.search_bool === "true"&&
+                        {this.state.success === "true" && this.state.search_bool === "false"&&
                         <h1 style={{textAlign: "center", marginTop: "25%"}}>No Videos Found</h1>}
                     </div>
                 </div>
