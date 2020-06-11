a6b2f508fabb4bcf6c3ce8a59e85d1d0e3813cd1
Shagun Agrawal <59168880+shagunagra@users.noreply.github.com>
2020-06-11 12:15:20 -0600

    Updates for icon show.

--- src/Comment_section/Display.js
+++ src/Comment_section/Display.js
@@ -3,11 +3,14 @@ import React from 'react';
 function Display(props){
 
     const { people } = props;
+    const {bool_state} = props;
     
     const peopleList = people.map( (person) => {
         return(
             <div id="comment_form" key={person.id}>
-               <span className="icon"><i class="fa fa-user" aria-hidden="true"></i></span>
+                {bool_state === "true" &&
+                 <span className="icon"><i class="fa fa-user" aria-hidden="true"></i></span>
+                }
                <span className="get_name"> {person.name} </span><br/>
                <span className="get_comment"> {person.comment} </span>
             </div>
