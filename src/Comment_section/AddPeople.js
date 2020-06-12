0d3ae33a77f9a30c1245b214fd0d3cbcd416a3c9
Shagun Agrawal <59168880+shagunagra@users.noreply.github.com>
2020-06-12 09:38:40 -0600

    This is my final commit.

--- src/Comment_section/AddPeople.js
+++ src/Comment_section/AddPeople.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './Comment.css';
 
+
+//this is add comment js file in which takes all the inputs of the comment user wants to give.
+
+
 class AddPerson extends Component{
+
+
     state = {
         name: null,
-        comment: null
+        comment: null,
     }
+
+    
+// function to take the id which each data will get
 
     handleChange = (e) => {
 
@@ -14,21 +23,30 @@ class AddPerson extends Component{
         })
     }
 
+
+
     handleSubmit = (e) =>{
         e.preventDefault();
         this.props.addPerson(this.state);
-
     }
 
+
+//function to cancel the given comment 
     handleClick = () => {
         document.getElementById('name').value = " ";
         document.getElementById('comment').value = " ";
       }
 
+
+
     render(){
         return(
             <div className="comment_content">
+
+
                 <p className="comment_section">Comments</p>
+
+
                 <form className="form">
                         <div className="input_content">
                             <div>
@@ -39,7 +57,10 @@ class AddPerson extends Component{
                             </div>
                         </div>
                 </form>
+
+
                 <button className="comment_btn" id="comment" onClick={this.handleSubmit}>Comment</button>
+                
                 <button className="cancel_btn" id="cancel" onClick={this.handleClick}>Cancel</button>
             </div>
         )
