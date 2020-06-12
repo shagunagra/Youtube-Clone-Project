86aaa0897b135bf9982726252425c200749458c6
Shagun Agrawal <59168880+shagunagra@users.noreply.github.com>
2020-06-12 09:37:47 -0600

    This is my final commit.

--- src/Main_call.js
+++ src/Main_call.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import 'font-awesome/css/font-awesome.min.css';
 import Home from './Home/Home';
 
+//this is the main js file which calls the home page.
+
+
 class Main extends Component{
 
     render(){
