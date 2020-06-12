da6cf7460ec2f5549cd4d5b3c36ef0a77fd6cffc
Shagun Agrawal <59168880+shagunagra@users.noreply.github.com>
2020-06-12 09:40:33 -0600

    This is my final commit.

--- src/Comment_section/Display.js
+++ src/Comment_section/Display.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
+//this js file dispalys the comment data on the screen.
+
 function Display(props){
+
 
     const { people } = props;
     const {set_bool} = props;
     
+
     const peopleList = people.map( (person) => {
         return(
+
             <div id="comment_form" key={person.id}>
-                {set_bool === "true" &&
+
+                {set_bool === "true" && person.id!==0 &&
                  <span className="icon"><i class="fa fa-user" aria-hidden="true"></i></span>
                 }
+
                <span className="get_name"> {person.name} </span><br/>
+               
                <span className="get_comment"> {person.comment} </span>
             </div>
         );
